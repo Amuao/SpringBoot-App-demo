e17a57dd0b4833bfaefeb4b372b7ab39b07fa122
amu <2397400600@qq.com>
2020-09-16 16:48:38 +0800

    添加redis集群测试

--- src/main/java/com/example/demo/controller/RedisTestController.java
+++ src/main/java/com/example/demo/controller/RedisTestController.java
@@ -1,70 +1,30 @@
 package com.example.demo.controller;
 
 import com.example.demo.core.base.Result;
-import com.example.demo.model.Country;
-import com.example.demo.service.CountryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+import java.util.Optional;
 
 @RestController
-public class CountryController {
-
+@Slf4j
+public class RedisTestController {
     @Autowired
-    private CountryService countryService;
+    private RedisTemplate<String, Serializable> redisTemplate;
 
-
-    /**
-     * 分页获取列表
-     *
-     * @return
-     */
-    @RequestMapping(value = "/countries", method = RequestMethod.GET)
-    public Result<Page<Country>> list(Integer currentPage, Integer pageSize) {
-        if(null == currentPage){
-            currentPage =1;
-            pageSize =10;
-        }
-        Pageable pageable =PageRequest.of(currentPage, pageSize,Sort.by(Sort.Direction.DESC,"id") );
-        return Result.success(countryService.findAll(pageable));
+    @RequestMapping(value = "/redis/add/{key}/{value}", method = RequestMethod.GET)
+    public Result<Boolean> add(@PathVariable("key")String key, @PathVariable("value")String value) throws Throwable {
+        ValueOperations<String, Serializable> opsForValue  = redisTemplate.opsForValue();
+        opsForValue.set(key,value);
+        Serializable o = Optional.ofNullable(opsForValue.get(key)).orElseThrow(Throwable::new);
+        log.info("redis add key = {} ,value = {} ",key,o );
+        return Result.success(true);
     }
-
-
-    /**
-     * 添加
-     *
-     * @return
-     */
-    @RequestMapping(value = "/countries", method = RequestMethod.POST)
-    public Result<Country> createCountry(@RequestBody @Validated Country country) {
-        return Result.success(countryService.add(country));
-    }
-
-    /**
-     * 修改
-     *
-     * @param country
-     * @return
-     */
-    @RequestMapping(value = "/countries", method = RequestMethod.PUT)
-    public Result<Country> update(Country country) {
-        return Result.success(countryService.update(country));
-    }
-
-    /**
-     * 获取单个
-     *
-     * @param id
-     * @return
-     */
-    @RequestMapping(value = "/countries/{id}", method = RequestMethod.GET)
-    public Result<Country> findOne(@PathVariable("id") Long id) {
-        return Result.success(countryService.get(id));
-    }
-
-
 }
--- src/main/java/com/example/demo/core/config/RedisConfig.java
+++ src/main/java/com/example/demo/core/config/RedisConfig.java
@@ -1,29 +1,58 @@
 package com.example.demo.core.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
-import org.springframework.scripting.support.ResourceScriptSource;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
 
 @Configuration
-public class AppConfig {
+public class RedisConfig {
+
     @Autowired
-    ClusterConfigurationProperties clusterProperties;
+    private ClusterConfigurationProperties clusterConfigurationProperties;
 
     @Bean
-    public  RedisConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory(clusterProperties.getNodes());
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        clusterConfigurationProperties.getNodes().forEach(s -> {
+                String[] url = s.split(":");
+                redisClusterConfiguration.clusterNode(url[0], Integer.parseInt(url[1]));
+            }
+        );
+        return new LettuceConnectionFactory(redisClusterConfiguration);
     }
 
-    @Bean
-    public DefaultRedisScript<String> redisScript() {
-        DefaultRedisScript<String> redisScript = new DefaultRedisScript<>();
-        redisScript.setResultType(String.class);
-        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("luascript/seckill.wlua")));
-        return redisScript;
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Serializable> redisTemplate() {
+        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+        template.setConnectionFactory(this.redisConnectionFactory());
+        RedisSerializer<?> stringSerializer = new StringRedisSerializer();
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化(默认采用的是JDK序列化)
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setKeySerializer(stringSerializer);
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(stringSerializer);
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
     }
 }
